89f72f05ae491e31948532f971de0a811b6decd7
kostia bazrow <osetia-alania@mail.ru>
2019-11-25 15:20:25 +0300

    stale-while-revalidate

--- public/sw-custom.js
+++ public/sw-custom.js
@@ -20,12 +20,32 @@ self.addEventListener('activate', e => {
     console.log('activate event => ', e)
 })
 
+/*
 self.addEventListener('fetch', e => {
     console.log('fetch intercepted for => ', e.request.url, e)
     e.respondWith( // waits for the innermost promise to resolve
         caches.match(e.request)
         .then(cachedResponse => {
             return cachedResponse || fetch(e.request) // <- the cache-first strategy
+        })
+    )
+})*/
+
+// => stale-while-revalidate:
+self.addEventListener('fetch', e => {
+    console.log('fetch stale-while-revalidate event => ', e.request.url, e)
+    e.respondWith(
+        caches.open(cacheName)
+        .then(cache => {
+            return cache.match(e.request)
+                .then(res => {
+                    const fetchPromise = fetch(e.request)
+                        .then(networkResp => {
+                            cache.puth(e.request, networkResp.clone())
+                            return networkResp
+                        })
+                    return response || fetchPromise
+                })
         })
     )
 })
---
+++ workbox-config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  "globDirectory": "dist/",
+  "globPatterns": [
+    "**/*.{css,ico,jpg,png,svg,jpeg,html,js,json,txt}"
+  ],
+  "swDest": "dist/sw.js"
+};
